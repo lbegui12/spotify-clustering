d2c5d453666c2e58c310dea804aa68d95b034519
lbegui12 <36458456+lbegui12@users.noreply.github.com>
2020-11-26 20:26:00 +0100

    Update secret.py

--- secret.py
+++ secret.py
@@ -6,8 +6,8 @@ Created on Wed Sep 30 14:48:41 2020
 """
 
 username="kmpr"
-spotify_user_id = "a482db196ece417b86425d3d6c157963"
-spotify_secret = "f62429dbc2684a9b921b90c841980618"
+spotify_user_id = "********************************"
+spotify_secret = "*********************************"
 
 
 scope = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private streaming ugc-image-upload user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-birthdate user-read-email user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played'
